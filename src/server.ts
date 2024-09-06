01140dd200a06999a0097aa9c1b18240d97b9d97
marceloaugusto33 <sjsjsjmarcelo@gmail.com>
2024-09-06 09:32:09 -0300

    feat: create the server

---
+++ src/server.ts
@@ -0,0 +1,27 @@
+import 'dotenv/config';
+import cors from 'cors';
+import express from 'express';
+
+export class Server {
+    server = express();
+    port: string | number
+
+    constructor() {
+        this.server = express();
+        this.port = process.env.PORT || 3000;
+
+        this.setConfigureServer();
+    }
+
+    setMiddlewares() {
+        this.server.use(express.json());
+        this.server.use(cors());
+        this.server.use(express.urlencoded({ extended: true }));
+    }
+
+    setConfigureServer() {
+        this.server.listen(this.port, () => {
+            console.log("SERVER RUNNING IN PORT", this.port);
+        })
+    }
+}
